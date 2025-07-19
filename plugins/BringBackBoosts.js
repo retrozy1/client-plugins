993829ee1b5e2f0601a7fee00f2fc7b1d3aa50c8
TheLazySquid <thelazysquid2@gmail.com>
2025-07-19 16:05:38 -0400

    Switch BringBackBoosts from parcel to rewriter

--- plugins/BringBackBoosts.js
+++ plugins/BringBackBoosts.js
@@ -2,7 +2,7 @@
  * @name BringBackBoosts
  * @description Restores boosts in Don't Look Down. Will cause you to desync, so others cannot see you move.
  * @author TheLazySquid
- * @version 0.4.5
+ * @version 0.5.0
  * @downloadUrl https://raw.githubusercontent.com/Gimloader/client-plugins/main/plugins/BringBackBoosts.js
  * @webpage https://gimloader.github.io/plugins/bringbackboosts
  * @needsLib DLDUtils | https://raw.githubusercontent.com/Gimloader/client-plugins/main/libraries/DLDUtils.js
@@ -53,45 +53,42 @@ api.net.onLoad((type) => {
     }
 });
 
-// The code used in this has been taken from minified Gimkit code and therefore is nearly unreadable.
-var r, g;
+var calcGravity;
+const calcGravCb = api.rewriter.createShared("CalculateGravity", (func) => {
+    calcGravity = func;
+});
 
-api.parcel.getLazy((exports) => exports?.default?.toString?.().includes(`physics.state.forces.some`), (exports) => {
-    r = exports;
-})
+api.rewriter.addParseHook("App", (code) => {
+    const index = code.indexOf("physics.state.forces.some");
+    if(index === -1) return;
 
-api.parcel.getLazy((exports) => exports?.isPlatformer && exports?.isEditingPlatformerAndPreferTopDownControls, (exports) => {
-    g = exports;
-})
+    const start = code.lastIndexOf(",", index) + 1;
+    const end = code.indexOf("=", start);
+    const name = code.slice(start, end);
+    code += `${calcGravCb}?.(${name});`;
 
-api.parcel.getLazy((exports) => exports?.CalculateMovementVelocity, exports => {
-    let nativeCalcVel = exports.CalculateMovementVelocity;
+    return code;
+});
 
-    var o = { CharacterPhysicsConsts: GL.platformerPhysics },
-        n = { default: GL.stores },
+
+const wrapCalcMovementVelocity = api.rewriter.createShared("WrapCalcMovmentVel", (func) => {
+    // The code used in this has been taken from minified Gimkit code and therefore is nearly unreadable.
+    var n = { default: GL.stores },
         a = { default: { normal: 310 }},
         I = { PhysicsConstants: {
             tickRate: 12,
             debug: !1,
             skipTilesDebug: !1
         }}
-
-    const C = A => {
-        const t = GL.stores.phaser.mainCharacter;
-        return t ? (0, g.isPlatformer)() && !(0, g.isEditingPlatformerAndPreferTopDownControls)() ? h(t, A.input) : l(t, A.input) : {
-            x: 0,
-            y: 0
-        }
-    },
-    h = (A, t) => {
+    const h = (A, t) => {
         let e = 0,
             i = 0;
         const s = null == t ? void 0 : t.angle,
             g = null !== s && (s < 90 || s > 270) ? "right" : null !== s && s > 90 && s < 270 ? "left" : "none",
             C = n.default.me.movementSpeed / a.default.normal;
-        let h = o.CharacterPhysicsConsts.platformerGroundSpeed * C;
+        let h = GL.platformerPhysics.platformerGroundSpeed * C;
         if (A.physics.state.jump.isJumping) {
-            const t = Math.min(o.CharacterPhysicsConsts.jump.airSpeedMinimum.maxSpeed, h * o.CharacterPhysicsConsts.jump.airSpeedMinimum.multiplier);
+            const t = Math.min(GL.platformerPhysics.jump.airSpeedMinimum.maxSpeed, h * GL.platformerPhysics.jump.airSpeedMinimum.multiplier);
             h = Math.max(t, A.physics.state.jump.xVelocityAtJumpStart)
         }
         let l = 0;
@@ -101,38 +98,36 @@ api.parcel.getLazy((exports) => exports?.CalculateMovementVelocity, exports => {
             A.physics.state.movement.accelerationTicks += 1;
             let t = 0,
                 i = 0;
-            A.physics.state.grounded ? B ? (t = o.CharacterPhysicsConsts.movement.ground.accelerationSpeed, i = o.CharacterPhysicsConsts.movement.ground.maxAccelerationSpeed) : t = o.CharacterPhysicsConsts.movement.ground.decelerationSpeed : B ? (t = o.CharacterPhysicsConsts.movement.air.accelerationSpeed, i = o.CharacterPhysicsConsts.movement.air.maxAccelerationSpeed) : t = o.CharacterPhysicsConsts.movement.air.decelerationSpeed;
+            A.physics.state.grounded ? B ? (t = GL.platformerPhysics.movement.ground.accelerationSpeed, i = GL.platformerPhysics.movement.ground.maxAccelerationSpeed) : t = GL.platformerPhysics.movement.ground.decelerationSpeed : B ? (t = GL.platformerPhysics.movement.air.accelerationSpeed, i = GL.platformerPhysics.movement.air.maxAccelerationSpeed) : t = GL.platformerPhysics.movement.air.decelerationSpeed;
             const s = 20 / I.PhysicsConstants.tickRate;
             t *= A.physics.state.movement.accelerationTicks * s, i && (t = Math.min(i, t)), e = l > A.physics.state.movement.xVelocity ? Phaser.Math.Clamp(A.physics.state.movement.xVelocity + t, A.physics.state.movement.xVelocity, l) : Phaser.Math.Clamp(A.physics.state.movement.xVelocity - t, l, A.physics.state.movement.xVelocity)
         } else e = l;
-        return A.physics.state.grounded && A.physics.state.velocity.y > o.CharacterPhysicsConsts.platformerGroundSpeed * C && Math.sign(e) === Math.sign(A.physics.state.velocity.x) && (e = A.physics.state.velocity.x), A.physics.state.movement.xVelocity = e, A.physics.state.gravity = (0, r.default)(A.id), i += A.physics.state.gravity, A.physics.state.forces.forEach(((A, t) => {
+        return A.physics.state.grounded && A.physics.state.velocity.y > GL.platformerPhysics.platformerGroundSpeed * C && Math.sign(e) === Math.sign(A.physics.state.velocity.x) && (e = A.physics.state.velocity.x), A.physics.state.movement.xVelocity = e, A.physics.state.gravity = calcGravity(A.id), i += A.physics.state.gravity, A.physics.state.forces.forEach(((A, t) => {
             const s = A.ticks[0];
             s && (e += s.x, i += s.y), A.ticks.shift()
         })), {
             x: e,
             y: i
         }
-    },
-    l = (A, t) => {
-        if (!t || null === t.angle) return {
-            x: 0,
-            y: 0
-        };
-        const e = n.default.me.movementSpeed / a.default.normal;
-        let i = o.CharacterPhysicsConsts.topDownBaseSpeed;
-        (0, g.isEditingPlatformerAndPreferTopDownControls)() && (i = o.CharacterPhysicsConsts.platformerGroundSpeed);
-        const s = i * e,
-            r = Phaser.Math.DegToRad(t.angle);
-        let I = Math.cos(r) * s,
-            C = Math.sin(r) * s;
-        return I = Math.round(1e3 * I) / 1e3, C = Math.round(1e3 * C) / 1e3, {
-            x: I,
-            y: C
-        }
     }
 
-    api.patcher.instead(exports, "CalculateMovementVelocity", (_, args) => {
-        if(!r || !g) return nativeCalcVel(...args);
-        return C(...args);
-    });
+    return function() {
+        if(GL.platformerPhysics && calcGravity && GL.plugins.isEnabled("BringBackBoosts")) {
+            return h(...arguments);
+        } else {
+            return func.apply(this, arguments);
+        }
+    }
+});
+
+api.rewriter.addParseHook("App", (code) => {
+    const index = code.indexOf("g.physics.state.jump.xVelocityAtJumpStart),");
+    if(index === -1) return;
+
+    const start = code.lastIndexOf("(", code.lastIndexOf("=>", index));
+    const end = code.indexOf("}}", code.indexOf("y:", index)) + 2;
+    const func = code.slice(start, end);
+    code = code.slice(0, start) + `(${wrapCalcMovementVelocity} ?? (v => v))(${func})` + code.slice(end);
+
+    return code;
 });
