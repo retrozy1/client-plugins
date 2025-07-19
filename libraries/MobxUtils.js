2380b16851e939242843c42072e4acbcb5e56c4a
TheLazySquid <thelazysquid2@gmail.com>
2025-07-19 18:00:06 -0400

    Switch MobxUtils from parcel to rewriter

--- libraries/MobxUtils.js
+++ libraries/MobxUtils.js
@@ -2,27 +2,47 @@
  * @name MobxUtils
  * @description Some simple utilities for react injection with MobX
  * @author TheLazySquid
- * @version 0.2.0
+ * @version 0.3.0
  * @downloadUrl https://raw.githubusercontent.com/Gimloader/client-plugins/main/libraries/MobxUtils.js
  * @isLibrary true
  * @reloadRequired true
  */
 
 const api = new GL();
-
 let observerIntercepts = [];
 
-api.parcel.getLazy(exports => exports && Object.keys(exports).length === 1 && exports?.observer, exports => {
-    api.patcher.before(exports, 'observer', (_, args) => {
-        // this is our only good way of telling apart functions
-        let str = args[0].toString();
-        for(let intercept of observerIntercepts) {
-            if(intercept.match(str)) {
-                let newVal = intercept.callback(args[0]);
-                if(newVal) args[0] = newVal;
+const wrapObserver = api.rewriter.createShared("ObserverWrapper", (func) => {
+    console.log("Wrapping", func);
+    return function() {
+        if(GL.libs.isEnabled("MobxUtils")) {
+            // this is our only good way of telling apart functions
+            let str = arguments[0].toString();
+            for(let intercept of observerIntercepts) {
+                if(intercept.match(str)) {
+                    let newVal = intercept.callback(arguments[0]);
+                    if(newVal) arguments[0] = newVal;
+                }
             }
         }
-    });
+
+        return func.apply(this, arguments);
+    }
+});
+
+api.rewriter.addParseHook("mobxreact", (code) => {
+    const index = code.indexOf("[mobx-react-lite]");
+    if(index === -1) return;
+
+    const funcStart = code.lastIndexOf("function", index);
+    const nameEnd = code.indexOf("(", funcStart);
+    const name = code.slice(funcStart + 9, nameEnd);
+    const funcEnd = code.indexOf("}", code.indexOf(".forwardRef", index)) + 1;
+    const func = code.slice(funcStart, funcEnd);
+
+    code = code.slice(0, funcStart) + `const ${name}=(${wrapObserver} ?? (v => v))(${func});`
+        + code.slice(funcEnd);
+
+    return code;
 });
 
 export function interceptObserver(id, match, callback) {
