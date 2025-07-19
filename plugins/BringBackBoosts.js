dee429a544678acd37ec44ad52ee42511e4fcb53
TheLazySquid <thelazysquid2@gmail.com>
2025-07-19 16:12:34 -0400

    Add missing reloadRequired header

--- plugins/BringBackBoosts.js
+++ plugins/BringBackBoosts.js
@@ -2,12 +2,13 @@
  * @name BringBackBoosts
  * @description Restores boosts in Don't Look Down. Will cause you to desync, so others cannot see you move.
  * @author TheLazySquid
- * @version 0.5.0
+ * @version 0.5.1
  * @downloadUrl https://raw.githubusercontent.com/Gimloader/client-plugins/main/plugins/BringBackBoosts.js
  * @webpage https://gimloader.github.io/plugins/bringbackboosts
  * @needsLib DLDUtils | https://raw.githubusercontent.com/Gimloader/client-plugins/main/libraries/DLDUtils.js
  * @needsLib QuickSettings | https://raw.githubusercontent.com/Gimloader/client-plugins/refs/heads/main/libraries/QuickSettings/build/QuickSettings.js
  * @hasSettings true
+ * @reloadRequired ingame
  */
 
 const api = new GL();
