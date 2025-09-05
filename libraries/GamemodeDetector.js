a7d6e17e3b22c7e320845331f9f4643153bbb1bb
retrozy <202344625+retrozy1@users.noreply.github.com>
2025-09-04 20:13:11 -0400

    Add missing gamemodes in GamemodeDetector
    Color Clash, Diamond Rush, and Apocalypse

--- libraries/GamemodeDetector.js
+++ libraries/GamemodeDetector.js
@@ -2,7 +2,7 @@
  * @name GamemodeDetector
  * @description Detects which official 2d gamemode the player is in
  * @author TheLazySquid
- * @version 0.1.0
+ * @version 0.2.0
  * @downloadUrl https://raw.githubusercontent.com/Gimloader/client-plugins/main/libraries/GamemodeDetector.js
  * @isLibrary true
  */
@@ -12,11 +12,14 @@ const gamemodeMusic = {
     "Fishtopia": '/assets/map/music/fishtopia/music.mp3',
     "Capture The Flag": '/assets/map/music/ctf/music.mp3',
     "Knockback": '/assets/map/modes/knockback/music.mp3',
+    "Color Clash": '/assets/map/modes/paint/music.mp3',
+    "Diamond Rush": '/assets/map/modes/lucky/music.mp3',
     "Dig It Up": '/assets/map/modes/mining/music.mp3',
     "One Way Out": '/assets/map/modes/oneWayOut/music.mp3',
     "Snowbrawl": '/assets/map/modes/snowbrawl/music.mp3',
     "Blastball": '/assets/map/modes/blastball/sound/music.mp3',
     "Snowy Survival": '/assets/map/modes/snowInfection/music.mp3',
+    "Apocalypse": '/assets/map/modes/zombie/music.mp3',
     "Tag": '/assets/map/music/tag/music.mp3',
     "Farmchain": '/assets/map/modes/farm/music.mp3'
 }
