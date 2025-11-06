8e5dd928526f43530a0571405bcacc71ca31e9fe
TheLazySquid <thelazysquid2@gmail.com>
2025-11-06 18:57:00 -0500

    Deprecate ShowHitboxes

--- build/plugins/ShowHitboxes.js
+++ build/plugins/ShowHitboxes.js
@@ -2,9 +2,10 @@
  * @name ShowHitboxes
  * @description Enables debug mode which shows hitboxes
  * @author TheLazySquid
- * @version 0.3.2
+ * @version 0.3.3
  * @downloadUrl https://raw.githubusercontent.com/Gimloader/client-plugins/main/plugins/ShowHitboxes.js
  * @webpage https://gimloader.github.io/plugins/showhitboxes
+ * @deprecated No longer works due to major changes by Gimkit.
  */
 
 const message = "ShowHitboxes no longer works due to major changes by Gimkit. Feel free to uninstall it.";
