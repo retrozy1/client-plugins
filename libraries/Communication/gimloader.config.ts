c300ba9991cc7aceede166463629569bf04531db
retrozy <202344625+retrozy1@users.noreply.github.com>
2025-11-17 09:21:22 -0500

    refactor: use the initial version that was used elsewhere

--- libraries/Communication/gimloader.config.ts
+++ libraries/Communication/gimloader.config.ts
@@ -5,7 +5,7 @@ export default {
     description: "Communication between different clients in 2D gamemodes",
     author: "retrozy",
     downloadUrl: "https://raw.githubusercontent.com/Gimloader/client-plugins/refs/heads/main/build/libraries/Communication.js",
-    version: "0.0.1",
+    version: "0.1.0",
     gamemodes: ["2d"],
     isLibrary: true
 };
