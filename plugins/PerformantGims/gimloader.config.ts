adfe2b8b80bd4d79730ff3d527102bd97c2f02c0
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-09-15 18:28:30 -0400

    Deprecate PerformantGims

--- plugins/PerformantGims/gimloader.config.ts
+++ plugins/PerformantGims/gimloader.config.ts
@@ -6,6 +6,7 @@ export default officialScriptConfig({
     description: "Replaces configurable gims with images of them. Looks like crap, runs really fast.",
     hasSettings: true,
     reloadRequired: "ingame",
-    version: "0.5.2",
-    changelog: ["Updated webpage url"]
+    version: "0.5.3",
+    changelog: ["Deprecated in favor of the Simple Gim Mode in base Gimkit"],
+    deprecated: "Superceded by Simple Gim Mode in base Gimkit"
 });
