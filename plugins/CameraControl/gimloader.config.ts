83403e324a591aa5371fdc8a139123240d05e5d3
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-09-14 21:49:02 -0400

    Bump version of some plugins to force updates
    Silly mistake

--- plugins/CameraControl/gimloader.config.ts
+++ plugins/CameraControl/gimloader.config.ts
@@ -4,8 +4,8 @@ export default officialScriptConfig({
     input: "src/index.ts",
     name: "CameraControl",
     description: "Lets you freely move and zoom your camera",
-    version: "1.1.0",
-    changelog: ["Exposed freecam functions for other scripts to use"],
+    version: "1.1.1",
+    changelog: ["Bumped version to force update"],
     hasSettings: true,
     optionalLibs: [
         "CommandLine | https://raw.githubusercontent.com/Blackhole927/gimkitmods/main/libraries/CommandLine/CommandLine.js"
