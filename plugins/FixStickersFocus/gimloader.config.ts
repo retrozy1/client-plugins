3aa1e538f180c9ef18d2de21ee27de79c7083fc4
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-09-16 11:39:08 -0400

    Deprecate FixStickerFocus

--- plugins/FixStickersFocus/gimloader.config.ts
+++ plugins/FixStickersFocus/gimloader.config.ts
@@ -5,5 +5,7 @@ export default officialScriptConfig({
     description: "Fixes the in-game stickers button keeping focus after being closed",
     input: "src/index.ts",
     reloadRequired: "ingame",
-    version: "0.1.0"
+    version: "0.1.1",
+    changelog: ["Deprecate since this bug has been fixed"],
+    deprecated: "This bug has been fixed by Gimkit"
 });
