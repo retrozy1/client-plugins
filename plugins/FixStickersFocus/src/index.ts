d89f416acb750ecc41d7fc5d5e5222bc0f2f6caf
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2026-09-16 11:40:06 -0400

    Remove FixStickerFocus

--- plugins/FixStickersFocus/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { replaceSection } from "$shared/rewritingUtils";
-
-api.rewriter.addParseHook("App", (code) => {
-    if(!code.includes("sticker.svg")) return code;
-    return replaceSection(code, ".sticker-drawer#onClick:@,", (onclick) => `() => { ${onclick}(); document.activeElement?.blur() }`);
-});
